14528e7a651b2e74872c365f9a8e5f2089fe4553
Miguel Flores <miguel.flores72623@gmail.com>
2024-05-10 06:27:03 -0400

    Add a function to input the operands

--- app.js
+++ app.js
@@ -1,12 +1,90 @@
 let display = "";
 
-let leftOperator = "";
-let rightOperator = "";
-let firstOperand = "";
-let secondOperand = "";
+let firstNumber = "";
+let secondNumber = "";
+let firstOperator = "";
+let secondOperator = "";
 
 const displayDiv = document.querySelector(".display");
 const buttons = document.querySelectorAll("button");
+
+function clearDisplay() {
+  firstNumber = "";
+  secondNumber = "";
+  firstOperator = "";
+  secondOperator = "";
+  display = "";
+}
+
+function populateDisplay() {
+  buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      if (btn.classList.contains("num")) {
+        // console.log("hello");
+        operand(btn.textContent);
+        updateDisplay();
+      } else if (btn.classList.contains("operator")) {
+        // console.log("operator");
+        operator(btn.textContent);
+        updateDisplay();
+      } else if (btn.classList.contains("equals")) {
+        // console.log("equals");
+equals()
+        updateDisplay();
+      } else if (btn.classList.contains("decimal")) {
+        console.log("decimal");
+        updateDisplay();
+      } else if (btn.classList.contains("delete")) {
+        console.log("delete");
+        updateDisplay();
+      } else if (btn.classList.contains("clear")) {
+        clearDisplay();
+        updateDisplay();
+      }
+    });
+  });
+}
+populateDisplay();
+
+function operand(num) {
+  if (firstOperator === "") {
+    if (display === "" || display === undefined) {
+      display = num;
+      console.log(display);
+    } else if (display === firstNumber) {
+      display = num;
+    } else {
+      display += num;
+    }
+  } else {
+    if (display === firstNumber) {
+      display = num;
+    } else {
+      display += num;
+    }
+  }
+}
+operand();
+
+// function operator(op) {
+//   firstNumber = display;
+//   firstOperator = op;
+//   console.log(firstOperator);
+// }
+
+// function equals() {
+//   if(firstOperator===''){
+//     display=display
+//   }else {
+//     secondNumber=display
+//     operate(firstOperator, secondOperator, firstOperator)
+//   }
+// }
+
+function updateDisplay() {
+  displayDiv.textContent = display;
+}
+updateDisplay();
 
 function add(numOne, numTwo) {
   return numOne + numTwo;
@@ -20,60 +98,6 @@ function multiply(numOne, numTwo) {
 function divide(numOne, numTwo) {
   return numOne / numTwo;
 }
-
-function clearDisplay() {
-  calculator.leftOperator = "";
-  calculator.rightOperator = "";
-  calculator.firstOperand = "";
-  calculator.secondOperand = "";
-}
-
-function populateDisplay() {
-  buttons.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      if (btn.classList.contains("num")) {
-        console.log("hello");
-        display += btn.textContent;
-        updateDisplay();
-      } 
-      else if (btn.classList.contains("operator")) {
-        console.log("operator");
-        updateDisplay();
-      }
-      else if (btn.classList.contains("equals")) {
-        console.log("equals");
-        updateDisplay();
-      }
-      else if (btn.classList.contains("decimal")) {
-        console.log("decimal");
-        updateDisplay();
-      }
-      else if (btn.classList.contains("delete")) {
-        console.log("delete");
-        updateDisplay();
-      }
-      else if (btn.classList.contains("clear")) {
-        console.log("clear");
-        updateDisplay();
-      }
-    });
-  });
-}
-populateDisplay();
-
-function operator() {
-  if ((leftOperator = "")) {
-    display = leftOperator;
-    console.log(display);
-  }
-}
-operator();
-
-
-function updateDisplay() {
-  displayDiv.textContent = display;
-}
-updateDisplay();
 
 function operate(leftNum, rightNum, operator) {
   if (operator === "+") {
